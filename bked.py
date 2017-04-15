81da92dc65dccf8b14a18148084d43f74d82aaf2
Zheng Luo <crescentluozheng@gmail.com>
2017-04-15 21:45:24 +0800

    20170415

--- bked.py
+++ bked.py
@@ -1,202 +1,119 @@
+"""
+An implementation of the kde bandwidth selection method outlined in:
+
+Reference: Z. I. Botev, J. F. Grotowski and D. P. Kroese
+            "KERNEL DENSITY ESTIMATION VIA DIFFUSION" ,Submitted to the
+            Annals of Statistics, 2009
+
+Based on the implementation in Matlab by Zdravko Botev.
+"""
+
+import scipy
 import numpy as np
-function [bandwidth,density,X,Y]=kde2d(data,n,MIN_XY,MAX_XY)
-# fast and accurate state-of-the-art
-# bivariate kernel density estimator
-# with diagonal bandwidth matrix.
-# The kernel is assumed to be Gaussian.
-# The two bandwidth parameters are
-# chosen optimally without ever
-# using/assuming a parametric model for the data or any "rules of thumb".
-# Unlike many other procedures, this one
-# is immune to accuracy failures in the estimation of
-# multimodal densities with widely separated modes (see examples).
-# INPUTS: data - an N by 2 array with continuous data
-#            n - size of the n by n grid over which the density is computed
-#                n has to be a power of 2, otherwise n=2^ceil(log2(n));
-#                the default value is 2^8;
-# MIN_XY,MAX_XY- limits of the bounding box over which the density is computed;
-#                the format is:
-#                MIN_XY=[lower_Xlim,lower_Ylim]
-#                MAX_XY=[upper_Xlim,upper_Ylim].
-#                The dafault limits are computed as:
-#                MAX=max(data,[],1); MIN=min(data,[],1); Range=MAX-MIN;
-#                MAX_XY=MAX+Range/4; MIN_XY=MIN-Range/4;
-# OUTPUT: bandwidth - a row vector with the two optimal
-#                     bandwidths for a bivaroate Gaussian kernel;
-#                     the format is:
-#                     bandwidth=[bandwidth_X, bandwidth_Y];
-#          density  - an n by n matrix containing the density values over the n by n grid;
-#                     density is not computed unless the function is asked for such an output;
-#              X,Y  - the meshgrid over which the variable "density" has been computed;
-#                     the intended usage is as follows:
-#                     surf(X,Y,density)
-# Example (simple Gaussian mixture)
-# clear all
-#   # generate a Gaussian mixture with distant modes
-#   data=[randn(500,2);
-#       randn(500,1)+3.5, randn(500,1);];
-#   # call the routine
-#     [bandwidth,density,X,Y]=kde2d(data);
-#   # plot the data and the density estimate
-#     contour3(X,Y,density,50), hold on
-#     plot(data(:,1),data(:,2),'r.','MarkerSize',5)
-#
-# Example (Gaussian mixture with distant modes):
-#
-# clear all
-#  # generate a Gaussian mixture with distant modes
-#  data=[randn(100,1), randn(100,1)/4;
-#      randn(100,1)+18, randn(100,1);
-#      randn(100,1)+15, randn(100,1)/2-18;];
-#  # call the routine
-#    [bandwidth,density,X,Y]=kde2d(data);
-#  # plot the data and the density estimate
-#  surf(X,Y,density,'LineStyle','none'), view([0,60])
-#  colormap hot, hold on, alpha(.8)
-#  set(gca, 'color', 'blue');
-#  plot(data(:,1),data(:,2),'w.','MarkerSize',5)
-#
-# Example (Sinusoidal density):
-#
-# clear all
-#   X=rand(1000,1); Y=sin(X*10*pi)+randn(size(X))/3; data=[X,Y];
-#  # apply routine
-#  [bandwidth,density,X,Y]=kde2d(data);
-#  # plot the data and the density estimate
-#  surf(X,Y,density,'LineStyle','none'), view([0,70])
-#  colormap hot, hold on, alpha(.8)
-#  set(gca, 'color', 'blue');
-#  plot(data(:,1),data(:,2),'w.','MarkerSize',5)
-#
-# Notes: If you have a more accurate density estimator 
-#        (as measured by which routine attains the smallest 
-#         L_2 distance between the estimate and the true density) or you have 
-#        problems running this code, please email me at botev@maths.uq.edu.au 
+from numpy.matlib import repmat
+from scipy.fftpack import dct, idct
 
 
-#  Reference: Z. I. Botev, J. F. Grotowski and D. P. Kroese
-#             "KERNEL DENSITY ESTIMATION VIA DIFFUSION" ,Submitted to the
-#             Annals of Statistics, 2009
+def kde2d(data, grid_n):
+    grid_n = 2** 8 if N is None else int(2**np.ceil(sci.log2(N)))
+    data_n = data.shape[0]
+    data_max = np.max(data, axis=0)
+    data_min = np.min(data, axis=0)
+    data_range = data_max - data_min
+    data_max_xy = data_max + data_range/4
+    data_min_xy = data_min - data_range/4
+    scaling = data_max_xy - data_min_xy
 
-grid_n = 2 ** 8
+    transformed_data=np.divide(data - repmat(data_min_xy, data_n, 1), repmat(scaling, data_n, 1))
+    #bin the data uniformly using regular grid;
+    init_data = np.histogram2d(transformed_data[0], transformed_data[1], grid_n)
+    # discrete cosine transform of initial data
+    SqrtDCTData = dct(init_data) ** 2
+    # now compute the optimal bandwidth^2
+    I = (np.arange(0, grid_n) **2).reshape(1,-1)
+    SqrtDCTData = a**2
+    t_star = scipy.optimize.brentq(fixed_point, 0, 0.1, 
+                                        args=(M, I, SqDCTData))
+def fixed_point(t, M, I, a2):
+        l=7
+        I = sci.float128(I)
+        M = sci.float128(M)
+        a2 = sci.float128(a2)
+        f = 2*sci.pi**(2*l)*sci.sum(I**l*a2*sci.exp(-I*sci.pi**2*t))
+        for s in range(l, 1, -1):
+            K0 = sci.prod(xrange(1, 2*s, 2))/sci.sqrt(2*sci.pi)
+            const = (1 + (1/2)**(s + 1/2))/3
+            time=(2*const*K0/M/f)**(2/(3+2*s))
+            f=2*sci.pi**(2*s)*sci.sum(I**s*a2*sci.exp(-I*sci.pi**2*time))
+        return t-(2*M*sci.sqrt(sci.pi)*f)**(-2/5)
+    t_star = 
+    t_star=fzero( @(t)(t-evolve(t)),[0,0.1]);
 
-grid_n = 2 ** np.ceil(log2(grid_n)) # round up n to the next power of 2;
-
-data_n = data.shape[0]
-
-data_max = np.max(data, axis=0)
-data_min = np.min(data, axis=0)
-data_range = data_max - data_min
-data_max_xy = data_max + Range/4
-data_min_xy = data_min - Range/4
-scaling=data_max_xy - data_min_xy
-
-
-transformed_data=(data-repmat(MIN_XY,N,1))./repmat(scaling,N,1);
-#bin the data uniformly using regular grid;
-initial_data=ndhist(transformed_data,n);
-# discrete cosine transform of initial data
-a= dct2d(initial_data);
-# now compute the optimal bandwidth^2
-  I=(0:n-1).^2; A2=a.^2;
-
- t_star=fzero( @(t)(t-evolve(t)),[0,0.1]);
-
-p_02=func([0,2],t_star);p_20=func([2,0],t_star); p_11=func([1,1],t_star);
-t_y=(p_02^(3/4)/(4*pi*N*p_20^(3/4)*(p_11+sqrt(p_20*p_02))))^(1/3);
-t_x=(p_20^(3/4)/(4*pi*N*p_02^(3/4)*(p_11+sqrt(p_20*p_02))))^(1/3);
-# smooth the discrete cosine transform of initial data using t_star
-a_t=exp(-(0:n-1)'.^2*pi^2*t_x/2)*exp(-(0:n-1).^2*pi^2*t_y/2).*a; 
-# now apply the inverse discrete cosine transform
-if nargout>1
-    density=idct2d(a_t)*(numel(a_t)/prod(scaling));
-    [X,Y]=meshgrid(MIN_XY(1):scaling(1)/(n-1):MAX_XY(1),MIN_XY(2):scaling(2)/(n-1):MAX_XY(2));
-end
-bandwidth=sqrt([t_x,t_y]).*scaling; 
-end
-########################################
-function  [out,time]=evolve(t)
-global N
-Sum_func = func([0,2],t) + func([2,0],t) + 2*func([1,1],t);
-time=(2*pi*N*Sum_func)^(-1/3);
-out=(t-time)/time;
-end
-########################################
-function out=func(s,t)
-global N
-if sum(s)<=4
-    Sum_func=func([s(1)+1,s(2)],t)+func([s(1),s(2)+1],t); const=(1+1/2^(sum(s)+1))/3;
-    time=(-2*const*K(s(1))*K(s(2))/N/Sum_func)^(1/(2+sum(s)));
-    out=psi(s,time);
-else
-    out=psi(s,t);
-end
-
-end
-########################################
-function out=psi(s,Time)
-global I A2
-# s is a vector
-w=exp(-I*pi^2*Time).*[1,.5*ones(1,length(I)-1)];
-wx=w.*(I.^s(1));
-wy=w.*(I.^s(2));
-out=(-1)^sum(s)*(wy*A2*wx')*pi^(2*sum(s));
-end
-########################################
-function out=K(s)
-out=(-1)^s*prod((1:2:2*s-1))/sqrt(2*pi);
-end
-########################################
-function data=dct2d(data)
-# computes the 2 dimensional discrete cosine transform of data
-# data is an nd cube
-[nrows,ncols]= size(data);
-if nrows~=ncols
-    error('data is not a square array!')
-end
-# Compute weights to multiply DFT coefficients
-w = [1;2*(exp(-i*(1:nrows-1)*pi/(2*nrows))).'];
-weight=w(:,ones(1,ncols));
-data=dct1d(dct1d(data)')';
-    function transform1d=dct1d(x)
-
-        # Re-order the elements of the columns of x
-        x = [ x(1:2:end,:); x(end:-2:2,:) ];
-
-        # Multiply FFT by weights:
-        transform1d = real(weight.* fft(x));
+    p_02=func([0,2],t_star);p_20=func([2,0],t_star); p_11=func([1,1],t_star);
+    t_y=(p_02^(3/4)/(4*pi*N*p_20^(3/4)*(p_11+sqrt(p_20*p_02))))^(1/3);
+    t_x=(p_20^(3/4)/(4*pi*N*p_02^(3/4)*(p_11+sqrt(p_20*p_02))))^(1/3);
+    # smooth the discrete cosine transform of initial data using t_star
+    a_t=exp(-(0:n-1)'.^2*pi^2*t_x/2)*exp(-(0:n-1).^2*pi^2*t_y/2).*a; 
+    # now apply the inverse discrete cosine transform
+    if nargout>1
+        density=idct2d(a_t)*(numel(a_t)/prod(scaling));
+        [X,Y]=meshgrid(MIN_XY(1):scaling(1)/(n-1):MAX_XY(1),MIN_XY(2):scaling(2)/(n-1):MAX_XY(2));
     end
-end
-########################################
-function data = idct2d(data)
-# computes the 2 dimensional inverse discrete cosine transform
-[nrows,ncols]=size(data);
-# Compute wieghts
-w = exp(i*(0:nrows-1)*pi/(2*nrows)).';
-weights=w(:,ones(1,ncols));
-data=idct1d(idct1d(data)');
-    function out=idct1d(x)
-        y = real(ifft(weights.*x));
-        out = zeros(nrows,ncols);
-        out(1:2:nrows,:) = y(1:nrows/2,:);
-        out(2:2:nrows,:) = y(nrows:-1:nrows/2+1,:);
+    bandwidth=sqrt([t_x,t_y]).*scaling; 
     end
-end
-########################################
-function binned_data=ndhist(data,M)
-# this function computes the histogram
-# of an n-dimensional data set;
-# 'data' is nrows by n columns
-# M is the number of bins used in each dimension
-# so that 'binned_data' is a hypercube with
-# size length equal to M;
-[nrows,ncols]=size(data);
-bins=zeros(nrows,ncols);
-for i=1:ncols
-    [dum,bins(:,i)] = histc(data(:,i),[0:1/M:1],1);
-    bins(:,i) = min(bins(:,i),M);
-end
-# Combine the  vectors of 1D bin counts into a grid of nD bin
-# counts.
-binned_data = accumarray(bins(all(bins>0,2),:),1/nrows,M(ones(1,ncols)));
-end
+
+    def evolve(t):
+        Sum_func = func([0, 2], t) + func([2, 0], t) + 2* func([1, 1], t)
+        time = (2 * np.pi * grid_n * Sum_func)^(-1/3)
+    ########################################
+    function  [out,time]=evolve(t)
+    global N
+    Sum_func = func([0,2],t) + func([2,0],t) + 2*func([1,1],t);
+    time=(2*pi*N*Sum_func)^(-1/3);
+    out=(t-time)/time;
+    end
+    ########################################
+    function out=func(s,t)
+    global N
+    if sum(s)<=4
+        Sum_func=func([s(1)+1,s(2)],t)+func([s(1),s(2)+1],t); const=(1+1/2^(sum(s)+1))/3;
+        time=(-2*const*K(s(1))*K(s(2))/N/Sum_func)^(1/(2+sum(s)));
+        out=psi(s,time);
+    else
+        out=psi(s,t);
+    end
+
+    end
+    ########################################
+    def psi(s, time)：
+
+    global I, SqrtDCTData
+    # s is a vector
+    w = np.exp(-I * np.pi ** 2 * time) * (0.5 * np.ones((1, len(I)-1)))
+    w=exp(-I*pi^2*Time).*[1,.5*ones(1,length(I)-1)];
+    wx=w.*(I.^s(1));
+    wy=w.*(I.^s(2));
+    out=(-1)^sum(s)*(wy*SqrtDCTData*wx')*pi^(2*sum(s));
+    return out
+    ########################################
+    function out=K(s)
+    out=(-1)^s*prod((1:2:2*s-1))/sqrt(2*pi);
+    end
+    ########################################
+    function binned_data=ndhist(data,M)
+    # this function computes the histogram
+    # of an n-dimensional data set;
+    # 'data' is nrows by n columns
+    # M is the number of bins used in each dimension
+    # so that 'binned_data' is a hypercube with
+    # size length equal to M;
+    [nrows,ncols]=size(data);
+    bins=zeros(nrows,ncols);
+    for i=1:ncols
+        [dum,bins(:,i)] = histc(data(:,i),[0:1/M:1],1);
+        bins(:,i) = min(bins(:,i),M);
+    end
+    # Combine the  vectors of 1D bin counts into a grid of nD bin
+    # counts.
+    binned_data = accumarray(bins(all(bins>0,2),:),1/nrows,M(ones(1,ncols)));
+    end
